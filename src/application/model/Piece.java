fdc5d9f8de4aa207e30cf83f536604c90942e7ec
Unknown <lombardoflorian@gmail.com>
2017-03-21 16:54:37 +0100

    src

---
+++ src/application/model/Piece.java
@@ -0,0 +1,68 @@
+package application.model;
+
+import java.util.ArrayList;
+
+public class Piece {
+	
+	private ArrayList<int[]> coord;
+	/**
+	 * Cette case permettra la rotation des pi�ces
+	 */
+	private int[] pivot;
+	
+	private String color;
+	
+	public Piece(ArrayList<int[]> coord){
+		this.setCoord(coord);
+	}
+	
+	public ArrayList<int[]> toLeft(){
+		ArrayList<int[]> left = new ArrayList();
+            for(int i = 0; i < 4; i++){
+                left.add(new int[]{this.getCoord().get(i)[0], this.getCoord().get(i)[1] - 1});
+            }
+		return left;
+	}
+	
+	public ArrayList<int[]> toRight(){
+            ArrayList<int[]> right = new ArrayList();
+            for(int i = 0; i < 4; i++){
+                right.add(new int[]{this.getCoord().get(i)[0], this.getCoord().get(i)[1] + 1});
+            }
+		return right;
+	}
+	
+	public ArrayList<int[]> toDown(){
+		ArrayList<int[]> down = new ArrayList();
+            for(int i = 0; i < 4; i++){
+                down.add(new int[]{this.getCoord().get(i)[0] + 1, this.getCoord().get(i)[1]});
+            }
+		return down;
+	}
+	
+	public ArrayList<int[]> rotate(){
+		
+		return null;
+	}
+
+	public ArrayList<int[]> getCoord() {
+		return coord;
+	}
+
+	public void setCoord(ArrayList<int[]> coord) {
+		this.coord = coord;
+	}
+	
+	public void setPivot(int [] pivot){
+		this.pivot = pivot;
+	}
+	
+	public String getColor(){
+		return color;
+	}
+	
+	public void setColor(String color){
+		this.color = color;
+	}
+
+}
---
+++ src/application/model/Tetris.java
@@ -0,0 +1,109 @@
+package application.model;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import application.controller.TetrisController;
+
+public class Tetris {
+	/**
+	 * Matrice de la grille
+	 */
+	private Piece[][] grid;
+
+	private Piece nextPiece;
+
+	private Piece moveablePiece;
+
+	private ArrayList<Piece> unmoveablePiece;
+
+	private String keyPressed;
+
+	private String[] pieces = { "S", "Z", "L", "J", "T", "O", "I" };
+
+	private TetrisController observer;
+
+	public Tetris() {
+		this.grid = new Piece[20][10];		
+
+		int rnd = new Random().nextInt(pieces.length);
+		this.nextPiece = PieceFactory.getPiece(pieces[rnd]); // Selection aleatoire de la piece
+		
+		rnd = new Random().nextInt(pieces.length);
+		this.moveablePiece = PieceFactory.getPiece(pieces[rnd]);
+
+		this.unmoveablePiece = new ArrayList<Piece>();
+		
+		this.setPosition(moveablePiece);
+	}
+
+	public void run() {
+
+		while (true) {
+			/*ArrayList<int[]> newCoord = moveablePiece.getCoord();
+
+			switch (keyPressed) {
+			case "UP":
+				newCoord = moveablePiece.rotate();
+				break;
+			case "LEFT":
+				newCoord = moveablePiece.toLeft();
+				break;
+			case "RIGHT":
+				newCoord = moveablePiece.toRight();
+				break;
+			}
+
+			if (!keyPressed.equals("") && checkPosition(newCoord)) {
+				moveablePiece.setCoord(newCoord);
+			}
+
+			keyPressed = "";
+
+			newCoord = moveablePiece.toDown();
+
+			if (checkPosition(newCoord)) {
+				moveablePiece.setCoord(newCoord);
+			} else {
+				unmoveablePiece.add(moveablePiece);
+				moveablePiece = nextPiece;
+
+				int rnd = new Random().nextInt(pieces.length);
+				nextPiece = PieceFactory.getPiece(pieces[rnd]);
+			}*/
+			
+			moveablePiece.setCoord(moveablePiece.toDown());
+			
+			this.setPosition(moveablePiece);
+			
+			this.notifyObserver();
+		}
+	}
+
+	private boolean checkPosition(ArrayList<int[]> coord) {
+		return true;
+	}
+
+	public void setKeyPressed(String keyPressed) {
+		this.keyPressed = keyPressed;
+	}
+
+	public void setObserver(TetrisController tetrisController) {
+		this.observer = tetrisController;
+	}
+
+	public void notifyObserver() {
+		observer.update();
+	}
+	
+	public Piece[][] getGrid(){
+		return grid;
+	}
+	
+	public void setPosition(Piece piece){
+		for(int[] coord : piece.getCoord()){
+			this.grid[coord[0]][coord[1]] = piece;
+		}
+	}
+
+}
